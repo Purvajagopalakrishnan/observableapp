c1df8f8e4c1ffbf8209619a120128c264fbd2c24
purvaja gopalakrishnan <purvajag@CESCHN.COM>
2019-01-25 21:52:47 +0530

    observable app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { isNgContainer } from '@angular/compiler';
+import { Observable, Observer } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'observable-app';
+export class AppComponent implements OnInit {
+ constructor(){
+}
+ngOnInit(){
+  this.start()
+}
+start(){
+  const observable = Observable.create((observer: Observer<string>)=>
+    {
+      setTimeout(() => {
+        observer.next( "first" );
+        },2000);
+        setTimeout(() => {
+        observer.next( "second" );
+        },4000);
+        setTimeout(() => {
+        observer.complete();
+        },5000);
+      }); 
+     observable.subscribe(
+        (data:string)=> {console.log(data)},
+        (error : string) => { console.log(error) } ,
+       // error => this.anyErrors = true,
+        () => { console.log("completed")}
+      );
+    }
 }
